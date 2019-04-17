3cd1c89a394aacb132ad6851bdcffc04e2b345de
Venkatesh Prasad V M <venkateshprasadvm@gmail.com>
2019-04-18 00:29:12 +0530

    Added Rick and Morty font and Reformated code

---
+++ lib/src/home/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Heello Rick'),
+      ),
+    );
+  }
+}
