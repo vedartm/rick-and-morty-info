bbb913e5a51b40836d3197374b851a05574f2241
Venkatesh Prasad V M <venkateshprasadvm@gmail.com>
2019-04-18 22:36:02 +0530

    Added pagination to Characters list

--- lib/src/characters/characters_page.dart
+++ lib/src/characters/characters_page.dart
@@ -9,25 +9,30 @@ class CharactersPage extends StatefulWidget {
 }
 
 class _CharactersPageState extends State<CharactersPage> {
+  int currentPage = 0;
+  List<dynamic> characters = <Character>[];
+
+/*
   @override
   Widget build(BuildContext context) {
     return Query(
-      "query {" +
-          " characters(page: , 1filter: { name: \"rick\" }) {" +
-          "   info {" +
-          "     count" +
-          "   }" +
-          "   results {" +
-          "     id" +
-          "     name" +
-          "     image" +
-          "     type" +
-          "     status" +
-          "     species" +
-          "     gender" +
-          "   }" +
-          " }" +
-          "}",
+      'query {' +
+          ' characters(page: $currentPage, filter: { name: \'morty\' }) {' +
+          '   info {' +
+          '     count' +
+          '     pages' +
+          '   }' +
+          '   results {' +
+          '     id' +
+          '     name' +
+          '     image' +
+          '     type' +
+          '     status' +
+          '     species' +
+          '     gender' +
+          '   }' +
+          ' }' +
+          '}',
       builder: ({
         bool loading,
         Map<String, dynamic> data,
@@ -41,17 +46,91 @@ class _CharactersPageState extends State<CharactersPage> {
           return const Center(child: CircularProgressIndicator());
         }
 
-        print(data);
-
         final List<dynamic> characters = data['characters']['results'];
         return ListView.builder(
           itemCount: characters.length,
-          itemBuilder: (BuildContext context, int index) {
+          ijtemBuilder: (BuildContext context, int index) {
             final Character character = Character.fromJson(characters[index]);
             return CharacterCard(character: character);
           },
         );
       },
     );
+  }
+*/
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return FutureBuilder<List<Map<String, dynamic>>>(
+          future: _queryCharacters(index),
+          builder: (BuildContext context,
+              AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.active:
+              case ConnectionState.waiting:
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height * 2,
+                  child: const Align(
+                      alignment: Alignment.topCenter,
+                      child: CircularProgressIndicator()),
+                );
+              case ConnectionState.done:
+                if (snapshot.hasError) {
+                  return Text('Error : ${snapshot.error}');
+                } else {
+                  final List<Map<String, dynamic>> page = snapshot.data;
+                  return _buildPage(page);
+                }
+            }
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildPage(List<Map<String, dynamic>> page) {
+    return ListView(
+      shrinkWrap: true,
+      primary: false,
+      children: page.map<Widget>((Map<String, dynamic> item) {
+        final Character character = Character.fromJson(item);
+        return CharacterCard(character: character);
+      }).toList(),
+    );
+  }
+
+  Client client = Client(
+    endPoint: 'https://rickandmortyapi.com/graphql',
+    cache: InMemoryCache(),
+  );
+
+  Future<List<Map<String, dynamic>>> _queryCharacters(int page) async {
+    final Map<String, dynamic> data = await client.query(
+      // query: readChars,
+      query: 'query {' +
+          ' characters(page: $page) {' +
+          '   info {' +
+          '     count' +
+          '     pages' +
+          '   }' +
+          '   results {' +
+          '     id' +
+          '     name' +
+          '     image' +
+          '     type' +
+          '     status' +
+          '     species' +
+          '     gender' +
+          '   }' +
+          ' }' +
+          '}',
+    );
+    print(data);
+    final List<Map<String, dynamic>> characters =
+        data['characters']['results'].cast<Map<String, dynamic>>();
+    return characters;
   }
 }
--- lib/src/widgets/characters_card.dart
+++ lib/src/widgets/characters_card.dart
@@ -9,7 +9,10 @@ class CharacterCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 120.0,
+        constraints: const BoxConstraints(
+          minHeight: 110,
+          maxHeight: 131,
+        ),
         margin: const EdgeInsets.symmetric(
           vertical: 16.0,
           horizontal: 24.0,
@@ -35,17 +38,58 @@ class CharacterCard extends StatelessWidget {
 
   Widget characterCard() {
     return Container(
-      height: 124.0,
       margin: const EdgeInsets.only(left: 46.0),
       decoration: BoxDecoration(
-        color: const Color(0xFFe89ac7),
+        // color: const Color(0xFFe89ac7),
+        color: Colors.blueAccent,
         shape: BoxShape.rectangle,
         borderRadius: BorderRadius.circular(8.0),
         boxShadow: const <BoxShadow>[
           BoxShadow(
             color: Colors.black12,
-            blurRadius: 10.0,
-            offset: Offset(0.0, 10.0),
+            blurRadius: 8.0,
+            offset: Offset(0.0, 8.0),
+          ),
+        ],
+      ),
+      child: cardContent(),
+    );
+  }
+
+  Widget cardContent() {
+    return Container(
+      alignment: FractionalOffset.center,
+      margin: const EdgeInsets.fromLTRB(66.0, 16.0, 16.0, 16.0),
+      constraints: const BoxConstraints.expand(),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            character.name,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Species - ${character.species}',
+            style: TextStyle(color: Colors.white70, fontSize: 18),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            children: <Widget>[
+              Text(
+                'Gender - ${character.gender}',
+                style: TextStyle(color: Colors.white70, fontSize: 16),
+              ),
+              const SizedBox(width: 16),
+              Text(
+                'Status - ${character.status}',
+                style: TextStyle(color: Colors.white70, fontSize: 16),
+              ),
+            ],
           ),
         ],
       ),
