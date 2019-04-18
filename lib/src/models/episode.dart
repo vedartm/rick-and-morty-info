cfd6d9adb7539aad8686e5e333b85638ab550314
Venkatesh Prasad V M <venkateshprasadvm@gmail.com>
2019-04-18 23:35:41 +0530

    Added Episode model

--- lib/src/models/episode.dart
+++ lib/src/models/episode.dart
@@ -1,1 +1,14 @@
+class Episode {
+  Episode({this.id, this.name, this.airDate, this.episode});
 
+  Episode.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        airDate = json['air_date'],
+        episode = json['episode'];
+
+  final String id;
+  final String name;
+  final String airDate;
+  final String episode;
+}
