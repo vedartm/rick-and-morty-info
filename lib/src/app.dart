f45522acab6e9041f26c9d969c1302ce90356b9b
Venkatesh Prasad V M <venkateshprasadvm@gmail.com>
2019-04-22 12:52:46 +0530

    Renamed the App

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Randm',
